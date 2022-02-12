923fad9355ca148c57e6574483c75fe31d44540c
Leszek Piątek <lpiatek@gmail.com>
2022-02-12 22:01:11 +0100

    ...

--- esphomeflasher/run.sh
+++ esphomeflasher/run.sh
@@ -8,12 +8,13 @@ OTA_BIN_FILE_PATH="$(jq --raw-output '.ota_bin_file_path' ${CONFIG_PATH})"
 OTA_PORT="$(jq --raw-output '.ota_port' ${CONFIG_PATH})"
 OTA_PASSWORD="$(jq --raw-output '.ota_password' ${CONFIG_PATH})"
 
-echo "Running: python3 /espota.py -i "${OTA_HOST}" -p "${OTA_PORT}" -a "${OTA_PASSWORD}" -f "${OTA_BIN_FILE_PATH}
-
 cat > /esphomeflasher.py << EOL
 from esphome import espota2
 espota2.run_ota("${OTA_HOST}", ${OTA_PORT}, "${OTA_PASSWORD}", "${OTA_BIN_FILE_PATH}")
 EOL
+
+echo "Running:"
+cat /esphomeflasher.py
 
 # exec python3 /espota.py -i "${OTA_HOST}" -p "${OTA_PORT}" -a "${OTA_PASSWORD}" -f "${OTA_BIN_FILE_PATH}"
 exec python3 /esphomeflasher.py
