0696ebf365ddef79873567a39e3540ccbb18bd81
Leszek Piątek <lpiatek@gmail.com>
2022-02-12 19:09:21 +0100

    ...

--- esphomeflasher/run.sh
+++ esphomeflasher/run.sh
@@ -3,8 +3,8 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
-OTA_HOST="$(jq --raw-output '.ota_host' ${CONFIG_PATH})",
-OTA_BIN_FILE_PATH="$(jq --raw-output '.ota_bin_file_path' ${CONFIG_PATH})",
+OTA_HOST="$(jq --raw-output '.ota_host' ${CONFIG_PATH})"
+OTA_BIN_FILE_PATH="$(jq --raw-output '.ota_bin_file_path' ${CONFIG_PATH})"
 OTA_PORT="$(jq --raw-output '.ota_port' ${CONFIG_PATH})"
 OTA_PASSWORD="$(jq --raw-output '.ota_password' ${CONFIG_PATH})"
 
